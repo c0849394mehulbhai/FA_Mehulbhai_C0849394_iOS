0a9b2ea3189662ab48fc9442a256234b19a18356
C0849394mehul <105397667+c0849394mehulbhai@users.noreply.github.com>
2022-05-30 19:09:15 -0400

    basic design

--- FA_Mehulbhai_C0849394_iOS/FA_Mehulbhai_C0849394_iOS/ViewController.swift
+++ FA_Mehulbhai_C0849394_iOS/FA_Mehulbhai_C0849394_iOS/ViewController.swift
@@ -2,17 +2,32 @@
 //  ViewController.swift
 //  FA_Mehulbhai_C0849394_iOS
 //
-//  Created by Dashmeen Kaur on 2022-05-30.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var a1: UIButton!
+    @IBOutlet weak var a2: UIButton!
+    @IBOutlet weak var a3: UIButton!
+    
+    @IBOutlet weak var b1: UIButton!
+    @IBOutlet weak var b2: UIButton!
+    @IBOutlet weak var b3: UIButton!
+    
+    @IBOutlet weak var c1: UIButton!
+    @IBOutlet weak var c2: UIButton!
+    @IBOutlet weak var c3: UIButton!
+    
+    @IBOutlet weak var PlayerX: UILabel!
+    @IBOutlet weak var PlayerO: UILabel!
+    
+    @IBOutlet weak var playerTurnLbl: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
 
 }
 
